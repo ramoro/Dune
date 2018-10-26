5903598cb9502545b02f32b3f0dc1de7c28b8226
ramoro <ram.santos@hotmail.com>
2018-10-26 18:15:25 -0300

    Delete canion.cpp

--- Armas/canion.cpp
+++
@@ -1,9 +0,0 @@
-#include "canion.h"
-#include <string>
-
-//DATOS DE INICIALIZACION HARCODEADOS (VAN A VENIR DEL ARCHIVO CONFIG)
-Canion::Canion() : Arma(7, 1) {
-	bonificaciones.insert(std::pair<std::string, int>("edificio", 0));
-	bonificaciones.insert(std::pair<std::string, int>("vehiculo", 0));
-	bonificaciones.insert(std::pair<std::string, int>("infanteria", 0));
-}
