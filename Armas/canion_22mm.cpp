43e421511eecc523e82dac82378094dc44c50882
ramoro <ram.santos@hotmail.com>
2018-10-26 18:15:38 -0300

    Delete canion_22mm.cpp

--- Armas/canion_22mm.cpp
+++
@@ -1,8 +0,0 @@
-#include "canion_22mm.h"
-
-//DATOS DE INICIALIZACION HARCODEADOS (VAN A VENIR DEL ARCHIVO CONFIG)
-Canion22mm::Canion22mm() : Arma(6, 6) {
-	bonificaciones.insert(std::pair<std::string, int>("edificio", 2));
-	bonificaciones.insert(std::pair<std::string, int>("vehiculo", 2));
-	bonificaciones.insert(std::pair<std::string, int>("infanteria", 0));
-}
