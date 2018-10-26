a45e5a353b78263e918b29f804200705edf70872
Ramiro Santos <ram.santos@hotmail.com>
2018-10-26 18:19:08 -0300

    nada nuevo

--- Armas/lanza_misiles.cpp
+++
@@ -1,8 +0,0 @@
-#include "lanza_misiles.h"
-
-//DATOS DE INICIALIZACION HARCODEADOS (VAN A VENIR DEL ARCHIVO CONFIG)
-LanzaMisiles::LanzaMisiles() : Arma(8, 1) {
-	bonificaciones.insert(std::pair<std::string, int>("edificio", 0));
-	bonificaciones.insert(std::pair<std::string, int>("vehiculo", 0));
-	bonificaciones.insert(std::pair<std::string, int>("infanteria", 0));
-}
--- Armas/onda_sonido.cpp
+++
@@ -1,8 +0,0 @@
-#include "onda_sonido.h"
-
-//DATOS DE INICIALIZACION HARCODEADOS (VAN A VENIR DEL ARCHIVO CONFIG)
-OndaSonido::OndaSonido() : Arma(4, 1) {
-	bonificaciones.insert(std::pair<std::string, int>("edificio", 0));
-	bonificaciones.insert(std::pair<std::string, int>("vehiculo", 0));
-	bonificaciones.insert(std::pair<std::string, int>("infanteria", 4));
-}
